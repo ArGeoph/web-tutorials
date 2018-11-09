4e7aa3fd99edb9f92819b8b82720a6b67e8a8d20
Kirill Golubev <arnasay@gmail.com>
2018-11-09 14:17:57 -0700

    Some improvements in UI. Quiz section added

---
+++ main.js
@@ -0,0 +1,6 @@
+// //Add or remove background and borders if menu item is selected 
+// document.querySelectorAll("nav > ul > li").addEventListener("click", toggleMenuButton, false);
+
+// const toggleMenuButton = (event) => {
+//     event.currentTarget.siblings
+// };
