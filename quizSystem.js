fe3b21ae8a8527fb584130f37ccb091a7d5339fc
Kirill Golubev <arnasay@gmail.com>
2018-11-13 12:07:20 -0700

    Problems wiht dropdown menu in Chrome fixed

--- quizSystem.js
+++ quizSystem.js
@@ -6,7 +6,7 @@ let submitButton, resetButton;
 const initialize = () => {
     quizContainer = document.getElementById("quiz1Container");
 
-    //load all questions from .json file
+    //load all questions from .json file and render quiz content after that
     loadQuiz().then(() => renderQuiz());
 };
 
@@ -14,10 +14,10 @@ const initialize = () => {
 //Asyncronous method to load all questions for the quiz
 const loadQuiz = async () => {
     try {
-        const response = await fetch("quizzes/quiz1.json");
+        const request = await fetch("quizzes/quiz1.json");
 
-        if (response.ok) {
-            const responseJson = await response.json();
+        if (request.ok) { //if request is successfull
+            const responseJson = await request.json();
             quizzes = responseJson.quiz;
         }
     }
@@ -30,8 +30,8 @@ const loadQuiz = async () => {
 const renderQuiz = async () => {
     let outputCode = "";
     console.log(quizzes);
-
-    if (quizzes.length === 0) {
+ 
+    if (quizzes.length === 0) { //If no quizes were added or there's so problems with the Internet print message
         outputCode = "<h2>Quizzes cannot be loaded at the moment. Please, check your Internet connection, or visit the page later";
     }
 
@@ -60,7 +60,7 @@ const renderQuiz = async () => {
 
 };
 
-//Method used to check the answers quiz 
+//Method used to check users answers
 const evaluateQuiz = () => {
     let questions = document.getElementsByClassName("optionsContainer");
     let numberOfQuestions = questions.length;
@@ -103,21 +103,24 @@ const evaluateQuiz = () => {
             }
         }
 
-        quizContainer.removeChild(document.getElementById("quiz"));
+        quizContainer.removeChild(document.getElementById("quiz")); //Remove quiz from the screen
         score = Math.round(((numberOfQuestions - wrongAnswerNumber)/numberOfQuestions)*100); //Calculate score
 
         let headingH3 = document.createElement("h3");
         let headingH2 = document.createElement("h2")
 
         if (wrongAnswerNumber > 0) { //If there's any wrong answers 
-            headingH2.appendChild(document.createTextNode(`The number of incorrect answers ${wrongAnswerNumber} out of ${numberOfQuestions}. Your score is ${score}%`));
+            headingH2.appendChild(document.createTextNode(`The number of incorrect answers ${wrongAnswerNumber} out of ${numberOfQuestions}. 
+                Your score is ${score}%`));
             quizContainer.appendChild(headingH2);
             headingH3.appendChild(document.createTextNode("The list of incorrect answers"));
             quizContainer.appendChild(headingH3);
 
-            quizContainer.appendChild(formObject);        }
-        else {
-            headingH2.appendChild(document.createTextNode(`Congratulations! You answered all ${numberOfQuestions} questions correctly! Your score is 100%`));
+            quizContainer.appendChild(formObject);        
+        }
+        else { //If user answered all questions correctly
+            headingH2.appendChild(document.createTextNode(`Congratulations! You answered all ${numberOfQuestions} questions correctly! 
+                Your score is 100%`));
             quizContainer.appendChild(headingH2);
         }
 
@@ -126,45 +129,10 @@ const evaluateQuiz = () => {
         '<a href="quiz1.html"><button class="mainPageButton">Repeat quiz</button></a>' + 
         '</div>';
     }
-    else {
+    else { //if user doesn't want to send their answers
         return;
     }
 
 };
-
-
-
-    //     <form id="quiz1">
-//     <div class="quizContainer" id="quiz1Question1">
-//         <h3>Question 1</h3>
-//         <p class="quizQuestion"> Which HTML element is used to put image on a webpage?</p>
-//         <div class="optionsContainer">
-//             <p><input type="radio" name="quiz1Question1Answer" value="p">&lt;p&gt;</p>
-//             <p><input type="radio" name="quiz1Question1Answer" value="a">&lt;a&gt;</p>
-//             <p><input type="radio" name="quiz1Question1Answer" value="image">&lt;image&gt;</p>
-//             <p><input type="radio" name="quiz1Question1Answer" value="img">&lt;img&gt;</p>
-//             <p><input type="radio" name="quiz1Question1Answer" value="div">&lt;div&gt;</p>
-//         </div>
-//     </div>
-
-//     <div class="quizContainer" id="quiz1Question2">
-//             <h3>Question 1</h3>
-//             <p class="quizQuestion"> Which HTML element is used to put image on a webpage?</p>
-//             <div class="optionsContainer">
-//                 <p><input type="radio" name="quiz1Question1Answer" value="p">&lt;p&gt;</p>
-//                 <p><input type="radio" name="quiz1Question1Answer" value="a">&lt;a&gt;</p>
-//                 <p><input type="radio" name="quiz1Question1Answer" value="image">&lt;image&gt;</p>
-//                 <p><input type="radio" name="quiz1Question1Answer" value="img">&lt;img&gt;</p>
-//                 <p><input type="radio" name="quiz1Question1Answer" value="div">&lt;div&gt;</p>
-//             </div>
-//     </div>                
-
-// <div class="buttonsContainer">
-//     <button id="quiz1SubmitButton" class="mainPageButton">Submit answers</button>
-//     <button id="quiz1SubmitButton" class="mainPageButton">Reset quiz</button>
-// </div>  
-// </form>   
-
-
 
 window.addEventListener("load", initialize, false); 
